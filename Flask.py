aa1a55c8a0cbb233780c88fd267237323b63d475
MuhammarShahzeb123 <shahzebnaveed621@gmail.com>
2023-04-22 00:42:42 +0500

    Stated Learning Flask

--- Flask.py
+++ Flask.py
@@ -2,17 +2,21 @@ from flask import Flask, render_template
 import requests
 import json
 
+
 root = Flask(__name__)
 def get_meme():
     url = "https://meme-api.com/gimme"
     responce = json.loads(requests.request("GET", url).text)
-    meme_large = responce['preview'][-2]
-    subreddit = responce['subreddit']
-    return subreddit, meme_large
+    subreddit = responce['url']
+    return subreddit
 
-@root.route('/')
+@root.route("/")
 def index():
-    meme_pic, subreddit = get_meme()
-    return render_template("index.html",meme_pic=meme_pic,subreddit=subreddit)
+    return "Hello, World!"
 
-root.run(host='0.0.0.0', port=5000)
+@root.route('/memes')
+def memes():
+    subreddit = get_meme()
+    return render_template("index.html",subreddit=subreddit)
+
+root.run(host='0.0.0.0', port=443, debug=True)
