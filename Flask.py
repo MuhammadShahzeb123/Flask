c83eaae316177fe51108f76376903e172ab437ca
MuhammarShahzeb123 <shahzebnaveed621@gmail.com>
2023-04-23 01:55:34 +0500

    Changed Name and Wasted a lot of time

--- Flask.py
+++ Flask.py
@@ -1,37 +1,29 @@
-from flask import Flask, render_template, request
-import Data
+from flask import Flask, render_template, url_for
+import Functions
 
 
 root = Flask(__name__)
 
-
-
-
 @root.route("/Home")
 def Home():
-    items = Data.Main()
-    return render_template('home.html', title="HOME", items = items.Result_data())
+    return render_template('home.html', title="HOME", items=Functions.dummy, content="Hello World")
 
-@root.route("/Ai")
-def ai():
-    return render_template('ChatGPT.html', title="ChatGPT")
 
-@root.route('/submit_text', methods=['POST'])
-def submit_text():
-    user_input = request.form['user_input']
-    return f"<h2>{user_input}</h2>"
+@root.route("/Layout")
+def Layout():
+    return render_template('layout.html', title="Layout", items=Functions.dummy)
 
 @root.route("/About")
 def about():
     return render_template('about.html', title="About")
 
-@root.route("/Code")
-def code():
-    return render_template('code.html', title="Code it")
+@root.route("/login")
+def login():
+    return "<h1>Under Progress</h1><div style='font-weight: bold; font-size: 20px; color: red'><a href='/Home'>Go Home</a></div>"
 
-@root.route('/Memes')
-def memes():
-    subreddit = Data.Main.get_meme()
-    return render_template("memes.html",subreddit=subreddit, title="Memes - Stay Happy")
+@root.route("/signup")
+def signup():
+    return "<h1>Under Progress</h1><div style='font-weight: bold; font-size: 20px; color: red'><a href='/Home'>Go Home</a></div>"
+
 
 root.run(host='0.0.0.0', port=80, debug=True)
---
+++ Functions.py
@@ -0,0 +1,55 @@
+import requests
+import json
+import openai
+import os
+import GPT_Key
+
+openai.api_key = GPT_Key.key
+
+class ChatGPT: 
+    def __init__(self, role) -> None:
+        self.Messages =  [
+                {"role": "system", "content": role}       
+            ]
+        
+    
+    
+    def ask(self, Q: str) -> str:
+        self.Messages.append({"role": "user", "content": Q})
+        
+        response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=self.Messages
+        ) 
+        self.Messages.append({"role": "assistant", "content": response["choices"][0]["message"]["content"]})
+        return response["choices"][0]["message"]["content"]
+    
+    
+    def Saving_Messages(self) -> None:
+        title = "Please Provide a 3 Worded Title for this conversation. NOTE: Only 3 Words and should described what we talked about."
+        Logs = self.ask(title)
+        with open(f"{Logs}.log", "a") as log:
+            for Message in self.Messages:
+                log.write(str(f"{Message}\n"))
+            log.write("\n\n\n")
+            log.close()
+        os.system("move *.log /logs")
+
+# Data i.e Lists, Sets, Other Stuff
+
+dummy = [
+    {
+        "name": "Shahzeb",
+        "class": "Metric",
+        "pic": "https://portal.fbise.edu.pk/fbise-conduct/std-img/img-ssc1/281200131.jpg",
+        "result": "https://portal.fbise.edu.pk/fbise-conduct/result/Result-link-ssc1.php?rollNo=9035206",
+        "marks": "475"
+    },
+    {
+        "name": "Zaryab",
+        "class": "Metric",
+        "pic": "https://portal.fbise.edu.pk/fbise-conduct/std-img/img-ssc1/281200119.jpg",
+        "result": "https://portal.fbise.edu.pk/fbise-conduct/result/Result-link-ssc1.php?rollNo=9035194",
+        "marks": "475"
+    }
+]
