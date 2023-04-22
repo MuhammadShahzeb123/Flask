b2e8b4e5ef6fd2e1c79208ebc32488ed17e019c7
MuhammarShahzeb123 <shahzebnaveed621@gmail.com>
2023-04-22 19:57:07 +0500

    Modified | Done some CSS

--- Data.py
+++ Data.py
@@ -6,12 +6,23 @@ class Main:
     def __init__(self) -> str:
         self.Titles = ['Home', 'About', 'Code']
 
+    def return_titles(self, num) -> str:
+        return self.Titles[num]
     
     def get_meme():
         url = "https://meme-api.com/gimme"
         response = json.loads(requests.request("GET", url).text)
         return response['url']
     
+    
 class ChatGPT:
     def __init__(self) -> None:
         pass
+    
+
+
+
+
+
+
+# Data i.e Lists, Sets, Other Stuff
--- Flask.py
+++ Flask.py
@@ -7,15 +7,23 @@ root = Flask(__name__)
 
 @root.route("/Home")
 def Home():
-    return render_template('home.html', title="Home")
+    return render_template('home.html', title="Home", items = [{'name':'Shahzeb', 'link':'https://portal.fbise.edu.pk/fbise-conduct/result/Result-link-ssc1.php?rollNo=9035206', 'marks': "475", 'class': 'Metric', 'pic': 'https://portal.fbise.edu.pk/fbise-conduct/std-img/img-ssc1/281200131.jpg'}])
 
-@root.route("/ai")
+@root.route("/Ai")
 def ai():
-    pass
+    return render_template('ChatGPT.html', title="Ai")
 
-@root.route('/memes')
+@root.route("/About")
+def about():
+    return render_template('about.html', title="About")
+
+@root.route("/Code")
+def code():
+    return render_template('code.html', title="Code")
+
+@root.route('/Memes')
 def memes():
     subreddit = Data.Main.get_meme()
-    return render_template("memes.html",subreddit=subreddit)
+    return render_template("memes.html",subreddit=subreddit, title="Memes")
 
 root.run(host='0.0.0.0', port=80, debug=True)
