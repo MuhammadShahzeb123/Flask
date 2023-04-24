6ea56452d8402ad88841c7b919c313df7838cb82
MuhammadShahzeb123 <shahzebnaveed621@gmail.com>
2023-04-25 01:05:21 +0500

    Added SignUp and Login

--- Flask.py
+++ Flask.py
@@ -1,26 +1,30 @@
 from flask import Flask, render_template, url_for, request
-from Functions import LoginForm, RegistrationForm
+from Functions import LoginForm, RegistrationForm, DataBase
 import Data
+
+database = DataBase()
 
 root = Flask(__name__)
 
 root.config['SECRET_KEY'] = '0b582cabd95b82f5ec0f4061826b4c36'
 
-@root.route("/Home")
-def Home():
+@root.route("/home")
+def home():
     return render_template('home.html', title="HOME", items=Data.dummy, content="Hello World")
 
 
-@root.route("/About")
+@root.route("/about")
 def about():
     return render_template('about.html', title="About")
 
 @root.route("/login", methods=["GET", "POST"])
 def login():
-    email = request.form.get("email")
-    password = request.form.get("password")
-    remember = request.form.get("remember")
-    print(f"{email} and {password} and {remember}")
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+        remember = request.form.get("remember")
+        print(email, password,)
+        # database.login(email, password)
     form = LoginForm()
     return render_template('login.html', title="Login", forms=form)
 
@@ -32,8 +36,11 @@ def signup():
         password = request.form.get("password")
         confirm_password = request.form.get("confirm_password")
         remember = request.form.get("remember")
-        print(f"{username} and {email} and {password} and {confirm_password} and {remember}")
+    
+        database.signup(username, email, password)
+    
     form = RegistrationForm()
     return render_template('signup.html', title="Signup", forms=form)
 
-root.run(host='0.0.0.0', port=80, debug=True)
+if __name__ == "__main__":
+    root.run(host='0.0.0.0', port=80, debug=True)
--- Functions.py
+++ Functions.py
@@ -4,6 +4,8 @@ import GPT_Key
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, HiddenField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 class RegistrationForm(FlaskForm):
         username = StringField("Username", validators=[DataRequired(),
@@ -22,10 +24,6 @@ class LoginForm(FlaskForm):
         forget_password = BooleanField('Forgot Password?')
         remember = BooleanField('Remember Me')
         submit = SubmitField('Login')
-
-
-
-
 
 openai.api_key = GPT_Key.key
 class ChatGPT: 
@@ -56,3 +54,36 @@ class ChatGPT:
                                 log.close()
                 os.system("move *.log /logs")
 
+class DataBase:
+        Engine = create_engine('sqlite:///Database.db') # It is the Instance that is used to create the database
+        Base = declarative_base() # This is the Base class in which Table can be Defined
+        
+        class User(Base):
+                __tablename__ = "Database"
+                id = Column(Integer, primary_key=True)
+                username = Column(String)
+                email = Column(String)
+                password = Column(String)
+        
+        Base.metadata.create_all(Engine)
+        
+        Session = sessionmaker(bind=Engine)
+        session = Session()
+
+        def signup(self, username, email, password) -> None:
+                User_Data_Query = self.session.query(self.User).filter_by(email=email).first()
+                if User_Data_Query.email == email:
+                        return "Already signed up"
+                else:
+                        User_Data = self.User(username=username, email=email, password=password)
+                        self.session.add(User_Data)
+                        self.session.commit()
+                        return "Signed up"
+
+                
+        def login(self, email, password) -> bool:
+                User_Data_Query = self.session.query(self.User).filter_by(email=email).first()
+                if User_Data_Query.email == email and User_Data_Query.password == password:
+                        return True
+                else:
+                        return False
